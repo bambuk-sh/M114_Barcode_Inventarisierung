3eccf733d5b030f6d6c53cbb29a5b40ee3042d67
pcaracal <peter.schreivogel@stud.kbw.ch>
2022-10-31 17:29:22 +0100

    getters and setters

--- src/main/java/ch/bbw/Inventarisierung.java
+++ src/main/java/ch/bbw/Inventarisierung.java
@@ -33,6 +33,7 @@ public class Inventarisierung {
 
     public void set_group(int group_key, String group_name){
         this.group.put(group_key, group_name);
+        this.group_object.put(group_key, new HashMap<Integer, String>());
     }
 
     public void set_group_object(int group_key, int object_key, String object_name){
@@ -45,5 +46,69 @@ public class Inventarisierung {
 
     public void set_area_room(int area_key, List<Integer> room_list){
         this.area_room.put(area_key, room_list);
+    }
+
+    public void setGroup_len(int group_len) {
+        this.group_len = group_len;
+    }
+
+    public void setObject_len(int object_len) {
+        this.object_len = object_len;
+    }
+
+    public void setArea_len(int area_len) {
+        this.area_len = area_len;
+    }
+
+    public void setRoom_len(int room_len) {
+        this.room_len = room_len;
+    }
+
+    public void setUid_min(int uid_min) {
+        this.uid_min = uid_min;
+    }
+
+    public void setUid_max(int uid_max) {
+        this.uid_max = uid_max;
+    }
+
+    public Map<Integer, String> getGroup() {
+        return group;
+    }
+
+    public Map<Integer, Map<Integer, String>> getGroup_object() {
+        return group_object;
+    }
+
+    public Map<Integer, String> getArea() {
+        return area;
+    }
+
+    public Map<Integer, List<Integer>> getArea_room() {
+        return area_room;
+    }
+
+    public int getGroup_len() {
+        return group_len;
+    }
+
+    public int getObject_len() {
+        return object_len;
+    }
+
+    public int getArea_len() {
+        return area_len;
+    }
+
+    public int getRoom_len() {
+        return room_len;
+    }
+
+    public int getUid_min() {
+        return uid_min;
+    }
+
+    public int getUid_max() {
+        return uid_max;
     }
 }
